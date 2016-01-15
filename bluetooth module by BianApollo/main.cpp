eae77a014047e8c2f1f39a881b5b1e6fe6ff1204
Apollo <apollo@apomarki.local>
2016-01-15 17:53:47 +0800

    Initial Commit

---
+++ bluetooth module by BianApollo/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  bluetooth module by BianApollo
+//
+//  Created by Apollo on 1/15/16.
+//  Copyright © 2016 Apollo. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
